a28028198ab315e4623ce46859851e6911856fe6
Revaz-Goguadze <goguadze.rezi2022@gmail.com>
2023-08-20 17:25:31 +0400

    adsasdaasdd

--- DoubleTransformer/GetIeee754Format.cs
+++ DoubleTransformer/GetIeee754Format.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using GenericMethods.Interfaces;
 
 namespace DoubleTransformer
@@ -9,13 +10,40 @@ namespace DoubleTransformer
     public class GetIeee754Format : ITransformer<double, string>
     {
         /// <summary>
-        /// Transform double value to IEEE754 format <see cref="https://www.wikiwand.com/en/IEEE_754"/> in the string form.
+        /// Transform double value to IEEE754 format <see>
+        ///     <cref>https://www.wikiwand.com/en/IEEE_754</cref>
+        /// </see>
+        /// in the string form.
         /// </summary>
         /// <param name="obj">The double value.</param>
         /// <returns>The IEEE754 format in the string form.</returns>
         public string Transform(double obj)
         {
-            throw new NotImplementedException();
+            DoubleToLongConverter converter = new DoubleToLongConverter(obj);
+            long longNumber = converter.LongTerm;
+            const int bitsInByte = 8;
+            const int bitsCount = sizeof(double) * bitsInByte;
+            char[] result = new char[bitsCount];
+            result[0] = longNumber < 0 ? '1' : '0';
+            for (int i = bitsCount - 2, j = 1; i >= 0; i--, j++)
+            {
+                result[j] = (longNumber & (1L << i)) != 0 ? '1' : '0';
+            }
+
+            return new string(result);
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        private readonly struct DoubleToLongConverter
+        {
+            [field: FieldOffset(0)]
+            public readonly long LongTerm;
+
+            [FieldOffset(0)]
+            private readonly double doubleTerm;
+
+            public DoubleToLongConverter(double doubleTerm)
+                : this() => this.doubleTerm = doubleTerm;
         }
     }
 }
